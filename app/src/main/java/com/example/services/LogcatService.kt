083162c086c9ee34768053e9f0ec9d2c6e374d59
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-08-02 11:39:05 +0430

    init notification on foreground service

--- app/src/main/java/com/example/services/LogcatService.kt
+++ app/src/main/java/com/example/services/LogcatService.kt
@@ -1,9 +1,15 @@
 package com.example.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 class LogcatService : Service() {
 
@@ -13,7 +19,12 @@ class LogcatService : Service() {
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        doMyJob()
+
+        startForeground(1110,getNotification())
+
+        Thread.sleep(8000)
+        stopSelf()
+
         return START_STICKY
     }
 
@@ -25,6 +36,7 @@ class LogcatService : Service() {
 
 
     override fun onDestroy() {
+        stopForeground(true )
         super.onDestroy()
     }
 
@@ -33,5 +45,23 @@ class LogcatService : Service() {
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    private fun getNotification():Notification{
+
+        val notificationManage=getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel=NotificationChannel("first Chanel","ChanelMusic",NotificationManager.IMPORTANCE_HIGH)
+            notificationManage.createNotificationChannel(notificationChannel)
+        }
+        val notification=NotificationCompat.Builder(this,"first Chanel")
+            .setSmallIcon(android.R.drawable.stat_notify_chat)
+            .setContentTitle("service running...")
+            .setContentText("Hacking :D ")
+            .build()
+
+        return notification
+
     }
 }
